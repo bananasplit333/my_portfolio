14e4e9519917d169a12747348e2a1d9c7a5535f0
bananasplit333 <134814180+bananasplit333@users.noreply.github.com>
2024-03-14 09:17:57 -0400

    updated files

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -8,7 +8,7 @@ import {
   Mastodon,
   Threads,
   Instagram,
-} from './icons'
+} from './icons';
 
 const components = {
   mail: Mail,
@@ -20,33 +20,37 @@ const components = {
   mastodon: Mastodon,
   threads: Threads,
   instagram: Instagram,
-}
+};
 
 type SocialIconProps = {
-  kind: keyof typeof components
-  href: string | undefined
-  size?: number
-}
+  kind: keyof typeof components;
+  href: string | undefined;
+  size?: number;
+};
 
 const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
-  if (!href || (kind === 'mail' && !/^mailto:\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(href)))
-    return null
+  if (
+    !href ||
+    (kind === 'mail' &&
+      !/^mailto:\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(href))
+  )
+    return null;
 
-  const SocialSvg = components[kind]
+  const SocialSvg = components[kind];
 
   return (
     <a
-      className="text-sm text-gray-500 transition hover:text-gray-600"
-      target="_blank"
-      rel="noopener noreferrer"
+      className='text-sm text-gray-500 transition hover:text-gray-600'
+      target='_blank'
+      rel='noopener noreferrer'
       href={href}
     >
-      <span className="sr-only">{kind}</span>
+      <span className='sr-only'>{kind}</span>
       <SocialSvg
         className={`fill-current text-gray-700 hover:text-primary-500 dark:text-gray-200 dark:hover:text-primary-400 h-${size} w-${size}`}
       />
     </a>
-  )
-}
+  );
+};
 
-export default SocialIcon
+export default SocialIcon;
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -11,14 +11,14 @@ const projectsData: Project[] = [
     description: `Simple UI to ask about weather conditions anywhere in the world. Utilizes OPENAI's API and built on 
     Django / Flask.`,
     imgSrc: '/static/images/weatherapp.png',
-    href: 'https://www.google.com',
+    href: '/https://github.com/bananasplit333/weatherapp',
   },
   {
     title: 'Hot Dog or Not Hot Dog',
     description: `Inspired by HBO Silicon Valley's Hot Dog application. Built on Python, uses machine learning
     to predict whether or not the image is a hot dog or not a hot dogs.`,
     imgSrc: '/static/images/hdnd.png',
-    href: '/blog/the-time-machine',
+    href: 'https://github.com/bananasplit333/hotdog_nothotdog/blob/main/Untitled.ipynb',
   },
   {
     title: 'Flashcard Maker',
