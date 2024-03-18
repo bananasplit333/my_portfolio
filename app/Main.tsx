538cc95b21bcc237446b881bf1f7383e177b4cdd
bananasplit333 <134814180+bananasplit333@users.noreply.github.com>
2024-03-18 00:38:10 -0400

    fixed bug

--- app/Main.tsx
+++ app/Main.tsx
@@ -37,7 +37,7 @@ export default function Home() {
                    about me 
                 </a>
               </RoughNotation>
-            </span> or {' '}
+            </span> or {'    '}
             <RoughNotation
                   show
                   type='circle'
