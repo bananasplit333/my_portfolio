4358bd6bf49e0dbdc54acc9f5e9293372e65000b
bananasplit333 <134814180+bananasplit333@users.noreply.github.com>
2024-04-13 15:15:46 -0400

    added loading animation, fixed border width

--- app/projects/weatherapp/page.tsx
+++ app/projects/weatherapp/page.tsx
@@ -9,6 +9,7 @@ import React from 'react';
 import { useState } from 'react';
 
 const WeatherApp: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [hasInput, setHasInput] = useState(false);
   const [inputValue, setInputValue] = useState('');
   const [showPlaceholder, setShowPlaceholder] = useState(true);
@@ -45,6 +46,10 @@ const WeatherApp: React.FC = () => {
 
   const handleSubmit = async () => {
     setHasInput(true);
+    setIsLoading(true);
+
+    // Delay the API call by 500ms (0.5 seconds)
+    await new Promise((resolve) => setTimeout(resolve, 500));
 
     try {
       console.log('submitted');
@@ -58,11 +63,7 @@ const WeatherApp: React.FC = () => {
       if (response.ok) {
         const data = await response.json();
         const weather_data = data.weather_data;
-        console.log(weather_data);
-        // Update the weatherData object with the data received from the API
-        console.log(weather_data.city_name);
-        console.log(weather_data.temperatures.current);
-        console.log(weather_data.wind.speed);
+        //set weather data  
         setWeatherData({
           icon: weather_data.icon,
           city: weather_data.city_name,
@@ -78,6 +79,8 @@ const WeatherApp: React.FC = () => {
       }
     } catch (error) {
       console.error('Error processing URL:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -107,24 +110,33 @@ const WeatherApp: React.FC = () => {
             <button onClick={handleSubmit}>Submit</button>
           </div>
         </div>
+      ) : isLoading ? (
+        <div className="my-40 flex">
+        <div className="relative mx-auto h-10 w-10 animate-bounce">
+          <div className="mx-auto h-16 w-16 animate-pulse rounded-full bg-gray-400"></div>
+          <span className="absolute flex h-5 w-5 animate-spin">
+            <span className="h-4 w-4 rounded-full bg-gray-400"> </span>
+          </span>
+        </div>
+</div>
       ) : (
         <div className="grid grid-cols-6">
           <div className="col-span-2">
-            <div className="bg-black-500 rounded-lg text-center">
+            <div className="rounded-lg bg-black-500 text-center">
               <div className="flex justify-center">
                 <img src={`/static/images/${weatherData.icon}.png`} alt="Weather Icon" />
               </div>
-              <div className="text-2xl font-bold mb-2">{weatherData.city}</div>
-              <div className="text-lg text-slate-50 mb-4">{weatherData.date}</div>
-              <div className="text-5xl font-bold mb-2">{weatherData.curr_temp}°</div>
-              <div className="text-l mb-4">feels like: {weatherData.feels_like}</div>
+              <div className="mb-2 text-2xl font-bold">{weatherData.city}</div>
+              <div className="mb-4 text-lg text-slate-50">{weatherData.date}</div>
+              <div className="mb-2 text-5xl font-bold">{weatherData.curr_temp}°</div>
+              <div className="mb-4 text-l">feels like: {weatherData.feels_like}</div>
               <div className="text-sm">max: {weatherData.max}</div>
-              <div className="text-sm"> min: {weatherData.min}</div>
+              <div className="text-sm">min: {weatherData.min}</div>
             </div>
           </div>
           <div className="col-span-4">
-            <div className="w-96 bg-black-500 rounded-lg mt-24">
-              <span ref={chatResponseRef} style={{ whiteSpace: 'pre-wrap' }} ></span>
+            <div className="mt-24 ml-4 w-64 rounded-lg bg-black-500">
+              <span ref={chatResponseRef} style={{ whiteSpace: 'pre-wrap' }}></span>
             </div>
           </div>
         </div>
