744375a8734b5d8dc1d11225d2b19a2ba2d8b113
bananasplit333 <134814180+bananasplit333@users.noreply.github.com>
2024-04-16 15:54:03 -0400

    added info

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -13,11 +13,21 @@ const projectsData: Project[] = [
     href: '/projects/weatherapp',
   },
   {
+    title: 'RecipeParser',
+    description: 'Enter a URL link and get the cooking instructions and ingredients. No more long winded stories, no more ugly clutter. Utilizes webscarping and NLP.',
+    href:'/projetcs/altfood'
+  },
+  {
     title: 'Hot Dog or Not Hot Dog',
     description: `Inspired by the hilarious "Not Hotdog" app from HBO's Silicon Valley. Built on Python, uses transfer learning
     to predict whether or not the image is a hot dog or not a hot dog.`,
     href: 'https://github.com/bananasplit333/hotdog_nothotdog/blob/main/Untitled.ipynb',
   },  
+  {
+    title: 'Image2Excel',
+    description: 'Utilizes OCR to extract key information from receipt images, and return a expense report in excel. Automates the annoying task of having to input your expenses manually.',
+    href:'https://receipt2excel.jaehyon.ca/'
+  },
   {
     title: 'Flashcard Maker',
     description: 'Upload any documents, receive flashcards in return to boost your exam success. Built using NextJS and Groq',
@@ -27,11 +37,6 @@ const projectsData: Project[] = [
     title: 'Pouch',
     description: 'Ecommerce site selling various goods. Built on NextJS, utilizes Shopify checkout',
     href:'https://www.pouch.fun'
-  },
-  {
-    title: 'Image2Excel',
-    description: 'Utilizes OCR to extract key information from receipt images, and return a expense report in excel. Automates the annoying task of having to input your expenses manually.',
-    href:'https://receipt2excel.jaehyon.ca/'
   },
   {
     title: 'Url_Shortener',
