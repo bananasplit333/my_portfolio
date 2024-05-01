e4a8d568e06eb0ee4c031161e383d4624fa445ba
bananasplit333 <134814180+bananasplit333@users.noreply.github.com>
2024-05-01 14:10:35 -0400

    test

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ScaleLoader from 'react-spinners/ScaleLoader';
+import RiseLoader from 'react-spinners/RiseLoader';
 
 interface ButtonProps {
   label: string;
@@ -8,15 +8,24 @@ interface ButtonProps {
   disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, isLoading = false, disabled = false }) => {
-  const opacity = disabled ? 'opacity-75' : 'opacity-100';
-  const cursor = disabled ? 'cursor-not-allowed' : 'cursor-pointer';
+const Button: React.FC<ButtonProps> = ({ label, onClick, isLoading}) => {
+
+  const Contents = isLoading ? (
+    <RiseLoader
+      color="#000"
+      size={6}
+      margin={3}
+    />
+  ) : (
+    <p>{label}</p>
+  )
 
   return (
     <button
       onClick={onClick}
-      className={`bg-white text-black border-2 border-gray-300 rounded-lg px-5 py-2 text-base outline-none shadow-md transition-all duration-300 ease-in-out ${opacity} ${cursor}`}
+      className={`bg-white text-black border-2 border-gray-300 rounded-lg px-5 py-2 text-base outline-none shadow-md transition-all duration-300 ease-in-out`}
     >
+      {Contents}
     </button>
   );
 };
--- components/ai_assistant/ActiveCallDetails.jsx
+++ components/ai_assistant/ActiveCallDetails.jsx
@@ -1,19 +1,8 @@
-import React from 'react';
 import AssistantSpeechIndicator from './call/AssistantSpeechIndicator';
 import Button from '../Button';
 import VolumeLevel from './call/VolumeLevel';
 
-interface ActiveCallDetailProps {
-  assistantIsSpeaking: boolean;
-  volumeLevel: number;
-  onEndCallClick: () => void;
-}
-
-const ActiveCallDetail: React.FC<ActiveCallDetailProps> = ({
-  assistantIsSpeaking,
-  volumeLevel,
-  onEndCallClick,
-}) => {
+const ActiveCallDetail = ({ assistantIsSpeaking, volumeLevel, onEndCallClick }) => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center p-4 border border-gray-300 rounded-lg shadow-md w-96 h-48">
