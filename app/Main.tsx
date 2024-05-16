c34f713f9ef3e8c98f6712718b2f7bb15076663c
bananasplit333 <jaehyon.yoo@gmail.com>
2024-05-16 14:32:00 -0400

    .

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,10 +1,5 @@
 import Link from '@/components/Link'
-import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
-import { formatDate } from 'pliny/utils/formatDate'
-import NewsletterForm from 'pliny/ui/NewsletterForm'
 import { RoughNotation } from 'react-rough-notation';
-import { gray, green } from 'tailwindcss/colors'
 
 const MAX_DISPLAY = 5
 
