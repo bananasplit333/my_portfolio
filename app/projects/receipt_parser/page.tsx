506df247b4d0c490b9908ba960bbb236b19b5dab
bananasplit333 <j.aehyon.yoo@gmail.com>
2024-05-16 14:46:59 -0400

    .

--- app/projects/receipt_parser/page.tsx
+++ app/projects/receipt_parser/page.tsx
@@ -2,7 +2,7 @@
 
 import DragAndDrop from '../../../components/MyDropZone';
 
-const ReceiptParser = () => {
+const ReceiptParser: React.FC = () => {
   return (
     <div className="flex flex-col justify-center">
       <DragAndDrop />
